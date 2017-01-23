81cf70c6106ea98219ac4624be3196abddb89473
Tellis Ellis <tbellisiv@gmail.com>
2017-01-23 13:01:41 -0600

    Added say-hello to -main.

--- code/sample-clj-proj/project.clj
+++ code/sample-clj-proj/project.clj
@@ -1,6 +1,6 @@
 (defproject sample-clj-proj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Sample Clojure Code for 'Emacs Coder Intro' presentation - https://www.gitbook.com/book/tbellisiv/clojure-emacs-cider-intro"
+  :url "https://github.com/tbellisiv/clojure-emacs-cider-intro/tree/master/code/sample-clj-proj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
--- code/sample-clj-proj/src/sample_clj_proj/core.clj
+++ code/sample-clj-proj/src/sample_clj_proj/core.clj
@@ -1,7 +1,14 @@
 (ns sample-clj-proj.core
   (:gen-class))
 
+(defn say-hello
+  "Prints a hello message. With no args, prints 'Hello!'. With one args prints 'Hello <arg>!. With two args prints 'Hello <arg1> from <arg>2!"
+  ([] (println "Hello!"))
+  ([to] (println (format  "Hello, %s!" to)))
+  ([to from] (println (format "Hello, %s from %s!" to from))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Main entrypoint"
   [& args]
-  (println "Hello, World!"))
+  (say-hello "EmacsATX" "Tellis"))
+
