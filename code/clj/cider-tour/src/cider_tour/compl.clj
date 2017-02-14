f715c3000a08d358e6a114c3862f70d46a4ba889
Tellis B. Ellis, IV <tbellisiv@gmail.com>
2017-02-14 13:49:50 -0600

    Added source file for Code Completion section

---
+++ code/clj/cider-tour/src/cider_tour/compl.clj
@@ -0,0 +1,9 @@
+(ns cider-tour.compl
+  (:require [cider-tour.core :refer [say-hello]]))
+
+;; On a new line type the string "(say-" (without quotes) and then type `C-M-i`.
+;; CIDER will expand "say-" to "say-hello" because there is only matching symbol.
+
+
+;; On a new line type the string "(ran" (without-quotes) and then type `C-M-i`.
+;; Since there are multiple possible matches, CIDER displays all possible matches in a popup buffer
