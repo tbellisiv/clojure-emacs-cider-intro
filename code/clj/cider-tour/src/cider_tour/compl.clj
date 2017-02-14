8711af462d35a570e6d6cafac57146d87f9c156f
Tellis B. Ellis, IV <tbellisiv@gmail.com>
2017-02-14 14:51:40 -0600

    Updates for Grimoire doc and code completion

--- code/clj/cider-tour/src/cider_tour/compl.clj
+++ code/clj/cider-tour/src/cider_tour/compl.clj
@@ -1,6 +1,8 @@
 (ns cider-tour.compl
   (:require [cider-tour.core :refer [say-hello]]))
 
+;; Type `C-c C-v C-n` to evaluate the above (ns ...) expression.
+
 ;; On a new line type the string "(say-" (without quotes) and then type `C-M-i`.
 ;; CIDER will expand "say-" to "say-hello" because there is only matching symbol.
 
--- code/clj/cider-tour/src/cider_tour/doc.clj
+++ code/clj/cider-tour/src/cider_tour/doc.clj
@@ -1,6 +1,8 @@
 (ns cider-tour.doc)
 
-;; Position point to println and type 'C-c C-d C-d [RET]' to view documentation for println
+;; Position point to "print" below and type 'C-c C-d C-d [RET]' to view the "docstring" documentation for print.
+;; Now, without moving point, type 'C-c C-d C-r [RET]' to view the Grimoire documentation in a buffer.
+;; Finally, type `C-c C-d C-w [RET]` to view the Grimoire documentation in a browser.
 (print "I'm here just for documentation\n.")
 
 ;; 4clojure Problem #27 - Palindrome Detector - https://www.4clojure.com/problem/27
